ed428525971ce19a6b5e551164a138368060a945
Ricky Johnson <ricky.johnson@techsoft3d.com>
2023-08-29 11:39:44 -0400

    added functionality for hc versioning

--- javascripts/conversionService.js
+++ javascripts/conversionService.js
@@ -36,4 +36,56 @@ async function startViewer() {
 
 }
 
+async function fetchVersionNumber() {
+        const conversionServiceURI = "https://csapi.techsoft3d.com";
 
+        let res = await fetch(conversionServiceURI + '/api/hcVersion');
+        var data = await res.json();
+        versionNumer = data;
+        
+        return data
+
+}
+
+async function initializeViewer() {
+        var screenConfiguration =
+      Sample.screenConfiguration == "mobile"
+        ? Communicator.ScreenConfiguration.Mobile
+        : Communicator.ScreenConfiguration.Desktop;
+
+    viewer = await startViewer()
+    viewer.setCallbacks({
+      selection: function (selection) {
+        propertyWindow.onSelection(selection);
+      },
+      sceneReady: function () {
+        viewer
+          .getView()
+          .setBackgroundColor(
+            new Communicator.Color(235, 235, 235),
+            new Communicator.Color(179, 179, 179)
+          );
+        var camera = viewer.getView().getCamera().copy();
+        var cam_pos = new Communicator.Point3(-100, 72, 390);
+        camera.setPosition(cam_pos);
+        camera.setUp(new Communicator.Point3(0.0, 1, 0));
+        camera.setWidth(1197.2799955801056);
+        camera.setHeight(1197.2799955801056);
+        viewer.getView().setCamera(camera, 0);
+        viewer.getSelectionManager().setHighlightLineElementSelection(false);
+        viewer.getSelectionManager().setHighlightFaceElementSelection(false);
+        viewer.getSelectionManager().setSelectParentIfSelected(false);
+        viewer.getView().setAmbientOcclusionEnabled(true);
+        viewer.getView().setAmbientOcclusionRadius(0.02);
+        viewer.setClientTimeout(60, 60);
+      },
+      modelStructureReady: function () {
+        initDemo();
+        enableControls();
+      },
+    });
+
+    window.onresize = function () {
+      viewer.resizeCanvas();
+    };
+}
--- javascripts/custom.js
+++ javascripts/custom.js
@@ -6,16 +6,16 @@ function collapsableWidgetFunctions() {
 
     if (isOn == false) {
       if ($('#NameOfPart').val() != '') {
-        html = '<img src="/demos/inventory-management/images/Collapse.png" class="collapse-image" style="opacity: 1.0" />';
+        html = '<img src="/stylesheets/images/Collapse.png" class="collapse-image" style="opacity: 1.0" />';
       } else {
-        html = '<img src="/demos/inventory-management/images/Collapse.png" class="collapse-image" style="opacity: 0.1" />';
+        html = '<img src="/stylesheets/images/Collapse.png" class="collapse-image" style="opacity: 0.1" />';
       }
       isOn = true;
     } else {
       if ($('#NameOfPart').val() != '') {
-        html = '<img src="/demos/inventory-management/images/Expand.png" class="collapse-image" style="opacity: 1.0" />';
+        html = '<img src="/stylesheets/images/Expand.png" class="collapse-image" style="opacity: 1.0" />';
       } else {
-        html = '<img src="/demos/inventory-management/images/Expand.png" class="collapse-image" style="opacity: 0.1" />';
+        html = '<img src="/stylesheets/images/Expand.png" class="collapse-image" style="opacity: 0.1" />';
       }
       isOn = false;
     }
